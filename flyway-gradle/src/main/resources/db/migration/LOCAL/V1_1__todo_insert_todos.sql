78b1b8535cea53a116cfdb6787f74866de0389c5
Andre Lehnert <andre.lehnert@exxcellent.de>
2020-08-04 21:21:12 +0200

    Revert "Create migration scripts"
    This reverts commit a549d19a

--- flyway-gradle/src/main/resources/db/migration/LOCAL/V1_1__todo_insert_todos.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO todo (id, task, created_at) VALUES (1, 'Hallo Welt', now()) ON CONFLICT DO NOTHING;
-INSERT INTO todo (id, task, created_at) VALUES (2, 'Flyway lernen', now()) ON CONFLICT DO NOTHING;
-INSERT INTO todo (id, task, created_at) VALUES (3, 'Datenbankmigration von A bis Z', now()) ON CONFLICT DO NOTHING;
